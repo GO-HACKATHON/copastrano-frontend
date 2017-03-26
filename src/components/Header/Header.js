6f061c1c13f82c3efa6f59b11092603b06d31bd2
Veri Ferdiansyah <veri_ferdi@outlook.com>
2017-03-26 08:39:21 +0700

    Update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,6 @@
  */
 
 import React from 'react';
-import { NavDropdown, MenuItem } from 'react-bootstrap';
 import Navbar, { Brand } from 'react-bootstrap/lib/Navbar';
 
 class Header extends React.Component {
@@ -18,19 +17,7 @@ class Header extends React.Component {
         <Navbar fluid style={{ margin: 0 }}>
           <Brand>
             <span>&nbsp;Admin Dashboard</span>
-            <button type="button" className="navbar-toggle" style={{ position: 'absolute', right: 0, top: 0 }}>
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-            </button>
           </Brand>
-          <ul className="nav navbar-top-links navbar-right">
-            <NavDropdown title={<i className="fa fa-user fa-fw" />} id="navDropdown">
-              <MenuItem eventKey="1">
-                <span><i className="fa fa-sign-out fa-fw" /> Logout </span>
-              </MenuItem>
-            </NavDropdown>
-          </ul>
         </Navbar>
       </div>
     );
